78c1113b93f4f461cd31e6d9ad09278e622394dc
unknown <saikatshen5@gmail.com>
2023-04-12 14:13:20 +0600

    Basic C

---
+++ Basic C/Setup/Welcome.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main ()
+{
+    printf("hello world\n");
+// int a=5,b=6;
+// int sum=a+b;
+// printf("%d\n",sum);
+
+    return 0;
+}
---
+++ Basic C/Setup/tempCodeRunnerFile.c
@@ -0,0 +1,3 @@
+int a=5,b=6;
+int sum=a+b;
+printf("%d\n%d\n",a,b);
